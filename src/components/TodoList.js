a50a5c0028c642f714d00dd8eca58b060294416f
dudcheol <95_pyc@naver.com>
2021-08-05 10:34:01 +0900

    map을 사용하여 컴포넌트로 변환할 때 key가 필요한 이유 주석 추가

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,6 +3,7 @@ import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
 // map을 사용하여 컴포넌트로 변환할 때는 key props를 전달해주어야 함
+// key가 필요한 이유? 요소의 변화를 알아차리기 위해
 const TodoList = ({todos}) => {
     return (
         <div className="TodoList">
