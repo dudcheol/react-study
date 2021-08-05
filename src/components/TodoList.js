cf3a35149f0254488fc61d286022d2e373e45452
dudcheol <95_pyc@naver.com>
2021-08-05 14:51:35 +0900

    TodoListItem에서 삭제 함수 호출하기

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,11 +4,11 @@ import './TodoList.scss';
 
 // map을 사용하여 컴포넌트로 변환할 때는 key props를 전달해주어야 함
 // key가 필요한 이유? 요소의 변화를 알아차리기 위해
-const TodoList = ({todos}) => {
+const TodoList = ({todos, onRemove}) => {
     return (
         <div className="TodoList">
             {todos.map(todo => (
-                <TodoListItem todo={todo} key={todo.id} />
+                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
             ))}
         </div>
     );
