91d87f2b1d99c3a701e769bfad43b3288a3de34a
dudcheol <95_pyc@naver.com>
2021-08-05 15:08:25 +0900

    onToggle 구현한 후 TodoListItem에서 토글 함수 호출하기

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,11 +4,11 @@ import './TodoList.scss';
 
 // map을 사용하여 컴포넌트로 변환할 때는 key props를 전달해주어야 함
 // key가 필요한 이유? 요소의 변화를 알아차리기 위해
-const TodoList = ({todos, onRemove}) => {
+const TodoList = ({todos, onRemove, onToggle}) => {
     return (
         <div className="TodoList">
             {todos.map(todo => (
-                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
+                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} />
             ))}
         </div>
     );
