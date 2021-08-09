656fbee22437476ee66c7caf453dc56ee2a66383
dudcheol <95_pyc@naver.com>
2021-08-09 14:51:47 +0900

    데이터 연동하기

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import axios from '../../node_modules/axios/index';
 import NewsItem from './NewsItem';
 
 const NewsListBlock = styled.div`
@@ -15,22 +16,50 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const sampleArticle = {
-    title: '제목',
-    description: '내용',
-    url: 'https://google.com',
-    urlToImage: 'https://via.placeholder.com/160',
-};
+// const sampleArticle = {
+//     title: '제목',
+//     description: '내용',
+//     url: 'https://google.com',
+//     urlToImage: 'https://via.placeholder.com/160',
+// };
 
 const NewsList = () => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(null);
+
+    // useEffect를 사용하여 컴포넌트가 처음 렌더링되는 시점에 API를 요청해본다
+    useEffect(() => {
+        // async를 사용하는 함수 따로 선언
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get(
+                    'https://newsapi.org/v2/top-headlines?country=kr&apiKey=db8c12a2fac84f129aabac184a20526d',
+                );
+                setArticles(response.data.articles);
+            } catch (e) {
+                console.log(e);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+    // 대기 중일 때
+    if (loading) {
+        return <NewsListBlock>대기 중...</NewsListBlock>
+    }
+    // 아직 articles 값이 설정되지 않았을 때
+    if (!articles) {
+        return null;
+    }
+    
+    // articles 값이 유효할 때
     return (
         <NewsListBlock>
-            <NewsItem article={sampleArticle}></NewsItem>
-            <NewsItem article={sampleArticle}></NewsItem>
-            <NewsItem article={sampleArticle}></NewsItem>
-            <NewsItem article={sampleArticle}></NewsItem>
-            <NewsItem article={sampleArticle}></NewsItem>
-            <NewsItem article={sampleArticle}></NewsItem>
+            {articles.map(article => (
+                <NewsItem key={ article.url } article={article}></NewsItem>
+            ))}
         </NewsListBlock>
     )
 };
