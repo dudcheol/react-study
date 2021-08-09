b8ffc40f08a2e10e640f75d96798dae2d77d36ca
dudcheol <95_pyc@naver.com>
2021-08-09 16:52:09 +0900

    Provider 사용하기

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import ColorBox from './components/ColorBox';
+import ColorContext from './contexts/color';
 
 function App() {
   return (
-    <ColorBox></ColorBox>
+    // Provider를 사용할 때는 value를 반드시 명시해야 제대로 작동함
+    <ColorContext.Provider value={{color:'red'}}>
+      <ColorBox></ColorBox>
+    </ColorContext.Provider>
   );
 }
 
--- context-tutorial/src/contexts/color.js
+++ context-tutorial/src/contexts/color.js
@@ -1,5 +1,5 @@
 import {createContext} from 'react';
 
-const ColorContext = createContext({color: 'black'});
+const ColorContext = createContext({color: 'black'}); // Provider를 사용하지 않았을 때만 사용됨
 
 export default ColorContext;
