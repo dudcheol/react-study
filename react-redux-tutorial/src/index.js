b64782d1eea1f7cab9e525c2610c1dc339023154
dudcheol <95_pyc@naver.com>
2021-08-11 23:46:21 +0900

    리액트 애플리케이션에 리덕스 적용하

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import rootReducer from './modules';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
+const store = createStore(rootReducer, composeWithDevTools());
+
+// 리액트 컴포넌트에서 스토어를 사용할 수 있도록 App 컴포넌트를 react-redux에서 제공하는 Provider 컴포넌트로 감싸줌
+// store를 props로 전달해주어야 함!
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
